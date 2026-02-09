c1e0af8a60b7a3109e7c2acbb29882f98a069f33
KuitoInoguchi <tstnamiou@gmail.com>
2026-02-09 15:53:22 +0800

    Finish 238 with concise preProduct approach

--- java-template/src/main/java/leetcode/editor/cn/ProductOfArrayExceptSelf.java
+++ java-template/src/main/java/leetcode/editor/cn/ProductOfArrayExceptSelf.java
@@ -8,49 +8,34 @@ public class ProductOfArrayExceptSelf {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] productExceptSelf(int[] nums) {
-            int[] answer = new int[nums.length];
-            PreProductArray preProd = new PreProductArray(nums);
-            PostProductArray postProd = new PostProductArray(nums);
-            for (int i = 0; i < nums.length; i++) {
-                int left = Math.max(0, i - 1);
-                int right = Math.min(i + 1, nums.length - 1);
-                answer[i] = preProd.getPreSum(left) * postProd.getPostSum(right);
+            int n = nums.length;
+
+            // prefix product array
+            int[] prefix = new int[n];
+            prefix[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                prefix[i] = prefix[i - 1] * nums[i];
             }
+
+            // suffix product array
+            int[] suffix = new int[n];
+            suffix[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                suffix[i] = suffix[i + 1] * nums[i];
+            }
+
+            // construct answer
+            int[] answer = new int[n];
+            answer[0] = suffix[1];
+            answer[n - 1] = prefix[n - 2];
+            for (int i = 1; i < n - 1; i++) {
+                answer[i] = prefix[i - 1] * suffix[i + 1];
+            }
+
             return answer;
         }
     }
 
-    class PreProductArray {
-        private int[] preProduct;
-
-        public PreProductArray(int[] nums) {
-            preProduct = new int[nums.length + 1];
-            preProduct[0] = 1;
-            for (int i = 1; i <= nums.length; i++) {
-                preProduct[i] = preProduct[i - 1] * nums[i - 1];
-            }
-        }
-
-        public int getPreSum(int to) {
-            return preProduct[to + 1];
-        }
-    }
-
-    class PostProductArray {
-        private int[] postProduct;
-
-        public PostProductArray(int[] nums) {
-            postProduct = new int[nums.length + 1];
-            postProduct[postProduct.length - 1] = 1;
-            for (int i = postProduct.length - 2; i >= 0; i--) {
-                postProduct[i] = postProduct[i + 1] * nums[i]; // nums[i + 1]: ArrayIndexOutOfBoundsException?
-            }
-        }
-
-        public int getPostSum(int to) {
-            return postProduct[to - 1];
-        }
-    }
     //leetcode submit region end(Prohibit modification and deletion)
 
     
