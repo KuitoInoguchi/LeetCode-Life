db8cca9538998d4ffb749a578c9eeb906ba2575b
KuitoInoguchi <tstnamiou@gmail.com>
2026-03-02 20:42:20 +0800

    Finish 445

---
+++ java-template/src/main/java/leetcode/editor/cn/FindKPairsWithSmallestSums.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+public class FindKPairsWithSmallestSums {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new FindKPairsWithSmallestSums().new Solution();
+        // put your test code here
+        
+    }
+}
