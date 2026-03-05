9432cc83bc4c185d4f7748b44be4df3dd7b08817
KuitoInoguchi <tstnamiou@gmail.com>
2026-03-05 11:36:02 +0800

    Finish 525

--- java-template/src/main/java/leetcode/editor/cn/ContiguousArray.java
+++ java-template/src/main/java/leetcode/editor/cn/ContiguousArray.java
@@ -8,7 +8,22 @@ public class ContiguousArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int findMaxLength(int[] nums) {
-            
+            int preSumLen = nums.length + 1;
+            int[] preSum = new int[preSumLen];
+            for (int i = 1; i < preSumLen; i++) {
+                preSum[i] = preSum[i - 1] + (nums[i - 1] == 0 ? -1 : 1);
+            }
+            Map<Integer, Integer> sumToIndex = new HashMap<>();
+            int res = 0;
+            for (int i = 0; i < preSumLen; i++) {
+                int sum = preSum[i];
+                if (!sumToIndex.containsKey(sum)) {
+                    sumToIndex.put(sum, i);
+                } else {
+                    res = Math.max(res, i - sumToIndex.get(sum));
+                }
+            }
+            return res;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
